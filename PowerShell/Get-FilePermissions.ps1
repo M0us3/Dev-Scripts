88b042fc867c7e4b4241048fcc6c7782b1812067
Nicolas Lindström <nicolaslindstrom1@gmail.com>
2023-08-18 10:41:59 -0700

    Create Get-FilePermissions.ps1

---
+++ PowerShell/Get-FilePermissions.ps1
@@ -0,0 +1,13 @@
+$FolderPath = dir -Directory -Path "c:\users\public\public documents\" -Recurse -Force
+$Report = @()
+Foreach ($Folder in $FolderPath) {
+    $Acl = Get-Acl -Path $Folder.FullName
+    foreach ($Access in $acl.Access)
+        {
+            $Properties = [ordered]@{'FolderName'=$Folder.FullName;'AD
+Group or
+User'=$Access.IdentityReference;'Permissions'=$Access.FileSystemRights;'Inherited'=$Access.IsInherited}
+            $Report += New-Object -TypeName PSObject -Property $Properties
+        }
+}
+$Report | Export-Csv -path "C:\Temp\FolderPermissions.csv"

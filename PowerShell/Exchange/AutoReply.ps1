94dd9e8c1dc39b5463c2f71c711a7b30e5fe3f7a
Nicolas Lindström <nicolaslindstrom1@gmail.com>
2025-04-26 10:49:00 -0700

    Update AutoReply.ps1

--- PowerShell/Exchange/AutoReply.ps1
+++ PowerShell/Exchange/AutoReply.ps1
@@ -1,4 +1,4 @@
-Set-MailboxAutoReplyConfiguration -Identity preaward@scripps.edu -AutoReplyState Scheduled -StartTime "11/22/2023 17:00:00" -EndTime "11/27/2023 04:00:00" -InternalMessage "Thank you for your email. The Scripps Research Institute is closed 11/23 and 11/24 in observance of the Thanksgiving Holiday. We will respond to all emails Monday, 11/27."
+Set-MailboxAutoReplyConfiguration -Identity testgroup@contoso.com -AutoReplyState Scheduled -StartTime "11/22/2023 17:00:00" -EndTime "11/27/2023 04:00:00" -InternalMessage "Thank you for your email. Contoso Support is closed for the holiday."
 
 
-Get-MailboxAutoReplyConfiguration -Identity preaward@scripps.edu
+Get-MailboxAutoReplyConfiguration -Identity testgroup@contoso.com

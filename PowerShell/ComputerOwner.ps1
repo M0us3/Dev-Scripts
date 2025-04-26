d00f260bd1d93054363ed46332c5374cc1abf26b
Nicolas Lindström <nicolaslindstrom1@gmail.com>
2025-04-26 10:51:26 -0700

    Update ComputerOwner.ps1

--- PowerShell/ComputerOwner.ps1
+++ PowerShell/ComputerOwner.ps1
@@ -1,1 +1,2 @@
+# This will pull the information of who created the computer object in Active Directory
 Get-ADComputer -identity $ComputerName | Select name,@{n="owner";e={(Get-acl "ad:\$($_.distinguishedname)").owner}}
